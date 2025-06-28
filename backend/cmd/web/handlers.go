94f38075f89e90180de7a7ae04a16df859fd6510
aschett <a.schett@outlook.de>
2025-06-28 14:14:06 +0200

    remove id logic

--- backend/cmd/web/handlers.go
+++ backend/cmd/web/handlers.go
@@ -3,16 +3,9 @@ package main
 import (
 	"encoding/json"
 	"net/http"
-	"strconv"
 )
 
 func (app *application) viewExercise(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.PathValue("id"))
-	if err != nil || id < 1 {
-		http.NotFound(w, r)
-		return
-	}
-
 	workouts := []map[string]interface{}{
 		{"id": 1, "title": "Chest Press", "weight": 32.5},
 	}
@@ -20,7 +13,7 @@ func (app *application) viewExercise(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 
-	err = json.NewEncoder(w).Encode(workouts)
+	err := json.NewEncoder(w).Encode(workouts)
 
 	if err != nil {
 		app.serverError(w, r, err)
