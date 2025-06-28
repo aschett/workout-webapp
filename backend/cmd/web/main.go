f5ce41374f0e095de4431c5f865edebbec51d331
aschett <a.schett@outlook.de>
2025-06-28 13:56:27 +0200

    add basics to build server

--- backend/cmd/web/main.go
+++ backend/cmd/web/main.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"flag"
+	"log/slog"
+	"net/http"
+	"os"
+)
+
+type application struct {
+	logger *slog.Logger
+}
+
 func main() {
 
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		AddSource: true,
+	}))
+
+	addr := flag.String("addr", ":4000", "Http network address")
+
+	logger.Info("Starting Server on", "addr", *addr)
+
+	app := &application{
+		logger: logger,
+	}
+
+	err := http.ListenAndServe(*addr, app.routes())
+	logger.Error(err.Error())
 }
---
+++ backend/cmd/web/routes.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"net/http"
+)
+
+func (app *application) routes() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	//REST API routes
+	return mux
+}
