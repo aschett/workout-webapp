634150c85338a7c2c2b751305b1ee7f4f3de35ff
aschett <a.schett@outlook.de>
2025-07-20 17:08:32 +0200

    Remove old backend

--- frontend_old/src/api/api-workout.jsx
+++
@@ -1,9 +0,0 @@
-export async function getAllWorkouts() {
-  const res = await fetch('/api/workouts');
-  return res.json();
-}
-
-export async function getWorkoutByID(id) {
-  const res = await fetch(`/api/workouts/${id}`);
-  return res.json();
-}
